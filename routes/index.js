2d303db887196830f87036a5d80a5594561a5e9c
Alexandre Tucunduva <motive.mobi@gmail.com>
2020-02-28 10:19:17 -0300

    setup inicial dos jogadores

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Batalha Naval' });
+  res.render('index', {
+    title: 'Batalha Naval',
+    nick: '',
+    userHash: '',
+  });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var md5 = require('md5');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/', function(req, res, next) {
+  let nick = req.body.nickname;
+  var hash = md5(nick);
+  //console.log(hash);
+  let user = [];
+  user = {
+    'userNick':nick,
+    'userHash':hash,
+  };
+  
+  res.render('index', {nick: nick,userHash: hash , title:'Batalha Naval'});
 });
 
 module.exports = router;
